08404c1adc47024b55951d97dee89547deec1201
kenneth <kenneth1.kodehode@gmail.com>
2023-01-16 09:13:41 +0100

    e

--- index.js
+++ index.js
@@ -30,19 +30,21 @@ client.on('messageCreate', message => {
             ["What about him?", "Who is that? Sounds like a cheater", "I dont like him", "Ahh, Joakim, thats the guy who steals other people's code, right?"]
         const response = responses[Math.floor(Math.random() * responses.length)]
         message.reply(`${client.user} ${response} `);
-// API
+// Joke API
     } else if (message.content.toLowerCase() === 'joke') {
         axios.get('https://jokeapi.dev/joke/Any')
         .then(response => {
         //   const category = response.data.category;
-          const joke = response.data.joke;
+          const joke = response.data.setup;
+          const delivery = response.data.delivery;
         //   const type = response.data.type;
-          message.reply(`${joke}`);
+          message.reply(`${joke}${delivery}`);
         })
         .catch(error => {
           console.log(error);
         });
     }
+        // Bored API
     else if (message.content.toLowerCase() === 'bored') {
         axios.get('http://www.boredapi.com/api/activity?participants=1').then(response => {
             const activity = response.data.activity;
