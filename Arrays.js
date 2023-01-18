a4335d98b4c7f1b178b707545a0418fbac753aee
kenneth <kenneth1.kodehode@gmail.com>
2023-01-18 23:14:21 +0100

    fixed joke api fetch and added jp

---
+++ Arrays.js
@@ -0,0 +1,41 @@
+const greetings = [
+    "Hello there!",
+    "Hi there!",
+    "What's up?",
+    "Yo!",
+    "Heya",
+    "Greetings!",
+    "Hallo - hva kan jeg hjelpe deg med idag?"];
+
+const responsesJoakim = [
+    "Why did Joakim cross the road? To steal code from the other side.",
+    "Joakim is a great programmer, he always write clean and efficient code",
+    "Joakim is a great leader, he always motivate his team to deliver the best results",
+    "Joakim is a true professional, he always delivers the projects on time",
+    "Joakim is a great problem solver, he always find the best solution for any challenge"];
+
+const responseJohn = ["He likes Mini-Party Biscuists",
+    "John is a hardworking individual who is dedicated to his job.",
+    "John is an excellent team leader, always willing to help others.",
+    "John has a great sense of humor and is a joy to be around.",
+    "John is highly skilled in his field and is always looking to improve.",
+    "John is a respected member of the community and is known for his generosity.",
+    "John is a family man who values his loved ones above all else.",
+    "John is a great listener and is always willing to offer advice.",
+    "John is a passionate and driven person who is determined to succeed.",
+    "John has a great sense of adventure and is always looking for new experiences.",
+    "John is a kind and compassionate person who is always willing to lend a helping hand."]
+
+
+
+
+
+
+
+
+
+module.exports = {
+    greetings: greetings,
+    responsesJoakim: responsesJoakim,
+    responseJohn: responseJohn
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js')
 require('dotenv/config')
-
+const arrays = require('./Arrays')
 // Library
 const axios = require('axios')
 
@@ -16,40 +16,46 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
     // console.log(client.user.id)
 })
-// Greetings
 client.on('messageCreate', message => {
-    const regex = /(hi|hello|hey|hei|hallo|halla|yo|heisann)/;
-    if (regex.test(message.content.toLowerCase()) && message.author.id !== client.user.id) {
-        const greetingArr = ["Hello there!", "Hi there!", "What's up?", "Yo!", "Heya", "Greetings!", "Hallo - hva kan jeg hjelpe deg med idag?"];
-        const svar = greetingArr[Math.floor(Math.random() * greetingArr.length)];
-        message.reply(`${svar}`)
-    }
-// Joakim   
-     else if (message.content.toLowerCase() === 'joakim') {
-        const responses =
-            ["What about him?", "Who is that? Sounds like a cheater", "I dont like him", "Ahh, Joakim, thats the guy who steals other people's code, right?"]
-        const response = responses[Math.floor(Math.random() * responses.length)]
-        message.reply(`${client.user} ${response} `);
-// Joke API
-    } else if (message.content.toLowerCase() === 'joke') {
-        axios.get('https://jokeapi.dev/joke/Any')
-        .then(response => {
-        //   const category = response.data.category;
-          const joke = response.data.setup;
-          const delivery = response.data.delivery;
-        //   const type = response.data.type;
-          message.reply(`${joke}\n\ ${delivery}`);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
+    if (message.author.id !== client.user.id) {
+        if (/(hey|hei|hi|hello|yo|halla|hallo)/.test(message.content.toLowerCase())) {
+            message.reply(arrays.greetings[Math.floor(Math.random() * arrays.greetings.length)]);
+        }
+
+        // Joakim    
+        else if (message.content.toLowerCase() === 'joakim') {
+            message.reply(arrays.responsesJoakim[Math.floor(Math.random() * arrays.responsesJoakim.length)]);
+
+            // John-Petter
+        }
+        else if (/(john petter|john-petter|john)/.test(message.content.toLowerCase())) {
+            message.reply(arrays.responseJohn[Math.floor(Math.random() * arrays.responseJohn.length)]);
+        }
+
+        // Joke API
+        else if (/(vits|si en vits|joke|fortell en vits|tell me a joke|gi meg en vits|)/.test(message.content.toLowerCase())) {
+            axios.get('https://jokeapi.dev/joke/Any')
+            .then(response => {
+              let joke;
+              if (response.data.setup) {
+                joke = `${response.data.setup}\n${response.data.delivery}`
+              } else {
+                joke = response.data.joke;
+              }
+              message.reply(joke);
+            })
+            .catch(error => {
+              console.log(error);
+            });          
+        }
+            
         // Bored API
-    else if (message.content.toLowerCase() === 'bored') {
-        axios.get('http://www.boredapi.com/api/activity?participants=1').then(response => {
-            const activity = response.data.activity;
-            message.reply(`${activity}`)
-        })
+        else if (message.content.toLowerCase() === 'bored') {
+            axios.get('http://www.boredapi.com/api/activity?participants=1').then(response => {
+                const activity = response.data.activity;
+                message.reply(`${activity}`)
+            })
+        }
     }
 });
 
