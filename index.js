479bd0ab4d5a578f05016919b9feeeffd106dc96
kenneth <kenneth1.kodehode@gmail.com>
2023-01-20 10:02:41 +0100

    fixed responses

--- index.js
+++ index.js
@@ -22,64 +22,64 @@ client.on('messageCreate', message => {
     if (message.author.id !== client.user.id) {
         if (/(hey|hei|hi|hello|yo|halla|hallo)/.test(message.content.toLowerCase())) {
             message.reply(arrays.greetings[Math.floor(Math.random() * arrays.greetings.length)]);
-        }
-// The Cat API
-        else if (/(katt|cat|pus|mjau)/.test(message.content.toLowerCase())) {
-            axios.get('https://api.thecatapi.com/v1/images/search', { headers: { 'x-api-key': (apiKeyCat) } })
-        .then(response => {
-        const imageUrl = response.data[0].url;
-        message.reply({ files: [imageUrl] });
-        })
-        .catch(error => {
-        console.log(error);
-});
-// The Dog API
-}        else if (/(dog|hund|)/.test(message.content.toLowerCase())) {
-        axios.get('https://api.thedogapi.com/v1/images/search', { headers: { 'x-api-key': (apiKeyDog) } })
-        .then(response => {
-        const imageUrl = response.data[0].url;
-        message.reply({ files: [imageUrl] });
-        })
-        .catch(error => {
-        console.log(error);
-        });
-
-}
-// Joakim    
+        }      else if (/(john petter|john-petter|john)/.test(message.content.toLowerCase())) {
+            message.reply(arrays.responseJohn[Math.floor(Math.random() * arrays.responseJohn.length)]);
+        }       
+        // Joakim
         else if (message.content.toLowerCase() === 'joakim') {
             message.reply(arrays.responsesJoakim[Math.floor(Math.random() * arrays.responsesJoakim.length)]);
 
-// John-Petter
-        }
-        else if (/(john petter|john-petter|john)/.test(message.content.toLowerCase())) {
-            message.reply(arrays.responseJohn[Math.floor(Math.random() * arrays.responseJohn.length)]);
-        }
 
-// Joke API
-        else if (/(vits|si en vits|joke|fortell en vits|tell me a joke|gi meg en vits|)/.test(message.content.toLowerCase())) {
+        }
+        // The Cat API
+        else if (/(katt|cat|pus|vis meg en katt)/.test(message.content.toLowerCase())) {
+            axios.get('https://api.thecatapi.com/v1/images/search', { headers: { 'x-api-key': (apiKeyCat) } })
+                .then(response => {
+                    const imageUrl = response.data[0].url;
+                    message.reply({ files: [imageUrl] });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            // The Dog API
+        } else if (/(dog|hund|vis meg en hund| show me a dog)/.test(message.content.toLowerCase())) {
+            axios.get('https://api.thedogapi.com/v1/images/search', { headers: { 'x-api-key': (apiKeyDog) } })
+                .then(response => {
+                    const imageUrl = response.data[0].url;
+                    message.reply({ files: [imageUrl] });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+
+        }           // Joke API
+        else if (/(vits|si en vits|joke|fortell en vits|tell me a joke|gi meg en vits)/.test(message.content.toLowerCase())) {
             axios.get('https://jokeapi.dev/joke/Any')
-            .then(response => {
-              let joke;
-              if (response.data.setup) {
-                joke = `${response.data.setup}\n${response.data.delivery}`
-              } else {
-                joke = response.data.joke;
-              }
-              message.reply(joke);
-            })
-            .catch(error => {
-              console.log(error);
-            });          
-        }
-
-            
-// Bored API
-        else if (message.content.toLowerCase() === 'bored') {
+                .then(response => {
+                    let joke;
+                    if (response.data.setup) {
+                        joke = `${response.data.setup}\n${response.data.delivery}`
+                    } else {
+                        joke = response.data.joke;
+                    } 
+                    message.reply(joke);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }   else if (message.content.toLowerCase() === 'bored') {
             axios.get('http://www.boredapi.com/api/activity?participants=1').then(response => {
                 const activity = response.data.activity;
                 message.reply(`${activity}`)
-            })
-        }
+                })
+            }
+  
+
+
+
+
+
+  
     }
 });
 
